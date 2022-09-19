3fa6c64aecd9dc20518f05d442541d3364951849
abeesdev <abeesdevjs@gmail.com>
2022-09-20 02:16:28 +0700

    setup docker and database

--- src/lib/DataSource.ts
+++ src/lib/DataSource.ts
@@ -2,7 +2,7 @@ import { DataSource } from 'typeorm';
 
 const AppDataSource = new DataSource({
   type: 'postgres',
-  host: process.env.DB_HOST,
+  // host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT),
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
@@ -13,7 +13,7 @@ const AppDataSource = new DataSource({
   // logging: true,
   extra: {
     ssl: {
-      rejectUnauthorized: true,
+      rejectUnauthorized: false,
     },
   },
 });
