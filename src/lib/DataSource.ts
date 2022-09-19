bbdbadad20cbfb8d316dbd966465e71bf1423544
abeesdev <abeesdevjs@gmail.com>
2022-09-20 04:54:33 +0700

    remove dist file

--- src/lib/DataSource.ts
+++ src/lib/DataSource.ts
@@ -2,13 +2,15 @@ import { DataSource } from 'typeorm';
 
 const AppDataSource = new DataSource({
   type: 'postgres',
+  host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT),
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  url: process.env.DATABASE_URL,
   entities: ['dist/entities/*.js'],
-  synchronize: false,
-  logging: true,
+  synchronize: true,
+  // logging: true,
   extra: {
     ssl: {
       rejectUnauthorized: false,
