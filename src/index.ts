2646b6a7b7c5f3d8d62c8b3bee6927b1158e3856
abeesdev <abeesdevjs@gmail.com>
2022-09-20 15:20:43 +0700

    setup

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import 'reflect-metadata';
 import express from 'express';
 import session from 'express-session';
-import AppDataSource from './lib/DataSource';
+// import AppDataSource from './lib/DataSource';
 import baseRouter from './routes/baseApi';
 import connectRedis from 'connect-redis';
 import cors from 'cors';
@@ -10,6 +10,8 @@ import http from 'http';
 import socketIo from './socket';
 import redis from './utils/redis';
 import passport from './lib/passport';
+import AppDataSource from './lib/DataSource';
+// import { createConnection } from 'typeorm';
 
 const main = async () => {
   const app = express();
@@ -17,6 +19,17 @@ const main = async () => {
   const RedisStore = connectRedis(session);
 
   await AppDataSource.connect();
+  // await createConnection({
+  //   type: 'postgres',
+  //   port: Number(process.env.DB_PORT),
+  //   username: process.env.DB_USER,
+  //   password: process.env.DB_PASSWORD,
+  //   database: process.env.DB_NAME,
+  //   url: process.env.DB_URL,
+  //   entities: ['dist/entities/*.js'],
+  //   synchronize: true,
+  //   logging: true,
+  // });
 
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
@@ -52,7 +65,7 @@ const main = async () => {
 
   socketIo(httpServer);
 
-  const PORT = process.env.PORT || 3080;
+  const PORT = process.env.PORT || 3081;
 
   httpServer.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}: http://localhost:${PORT}`);
--- src/lib/DataSource.ts
+++ src/lib/DataSource.ts
@@ -2,20 +2,19 @@ import { DataSource } from 'typeorm';
 
 const AppDataSource = new DataSource({
   type: 'postgres',
-  host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT),
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  url: process.env.DATABASE_URL,
+  url: process.env.DB_URL,
   entities: ['dist/entities/*.js'],
   synchronize: true,
-  // logging: true,
-  extra: {
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  },
+  logging: true,
+  // extra: {
+  //   ssl: {
+  //     rejectUnauthorized: false,
+  //   },
+  // },
 });
 
 export default AppDataSource;
